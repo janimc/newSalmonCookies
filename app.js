611f55b449f3c8776db2f5c2313c8076ce3b00ac
Johannes Michael <janimc@gmail.com>
2018-09-29 14:04:07 -0700

    Dom Created for lab 6

--- app.js
+++ app.js
@@ -27,13 +27,212 @@ var pikePlace = {
   },
 
   listThings: function () {
-    car.storesContainer = document.createElement('div')
+    var container = document.createElement('div')
     container.innerHTML = '<h3>' + this.name + '<h3>'
     document.body.appendChild(container);
+
+    var list = document.createElement('ul')
+    var listArr = [];
+
+    for (var i = 0; i < workHours.length; i++){
+      if (i<5) {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+      } else {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+        console.log('BEFORE',listArr);
+
+      }
+    }
+    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
+
+    var fullString = listArr.join('');
+    list.innerHTML = fullString;
+    document.body.appendChild(list);
+    console.log('AFTER',listArr);
+
   }
 
-}
+};
 
 pikePlace.calculateCustomersPerHour();
 pikePlace.calculateCookiesSoldEachHour();
+pikePlace.listThings();
 
+
+var seatacAirport = {
+
+  name: 'SeaTac Airport',
+  min: 3,
+  max: 24,
+  avgCookiesPerSale: 1.2,
+  cookiesSoldEachHour: [],
+  avgCustArr: [],
+  totalCooks: 0,
+  //Constructor for all stores
+  calculateCustomersPerHour: function () {
+    for (var i = 0; i < workHours.length; i++) {
+      var randomAmount = Math.floor(Math.random() * (this.max - this.min)) + this.min;
+      this.avgCustArr.push(randomAmount);
+      console.log(this.avgCustArr);
+    }
+  },
+
+  calculateCookiesSoldEachHour: function () {
+    for (var j = 0; j < workHours.length; j++) {
+      this.totalCooks += this.avgCustArr[j];
+      console.log('***********', this.totalCooks)
+    }
+  },
+
+  listThings: function () {
+    var container = document.createElement('div')
+    container.innerHTML = '<h3>' + this.name + '<h3>'
+    document.body.appendChild(container);
+
+    var list = document.createElement('ul')
+    var listArr = [];
+
+    for (var i = 0; i < workHours.length; i++){
+      if (i<5) {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+      } else {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+        console.log('BEFORE',listArr);
+
+      }
+    }
+    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
+
+    var fullString = listArr.join('');
+    list.innerHTML = fullString;
+    document.body.appendChild(list);
+    console.log('AFTER',listArr);
+
+  }
+
+};
+
+seatacAirport.calculateCustomersPerHour();
+seatacAirport.calculateCookiesSoldEachHour();
+seatacAirport.listThings();
+
+
+
+
+var capitolHill = {
+
+  name: 'Capitol Hill',
+  min: 20,
+  max: 38,
+  avgCookiesPerSale: 2.3,
+  cookiesSoldEachHour: [],
+  avgCustArr: [],
+  totalCooks: 0,
+  //Constructor for all stores
+  calculateCustomersPerHour: function () {
+    for (var i = 0; i < workHours.length; i++) {
+      var randomAmount = Math.floor(Math.random() * (this.max - this.min)) + this.min;
+      this.avgCustArr.push(randomAmount);
+      console.log(this.avgCustArr);
+    }
+  },
+
+  calculateCookiesSoldEachHour: function () {
+    for (var j = 0; j < workHours.length; j++) {
+      this.totalCooks += this.avgCustArr[j];
+      console.log('***********', this.totalCooks)
+    }
+  },
+
+  listThings: function () {
+    var container = document.createElement('div')
+    container.innerHTML = '<h3>' + this.name + '<h3>'
+    document.body.appendChild(container);
+
+    var list = document.createElement('ul')
+    var listArr = [];
+
+    for (var i = 0; i < workHours.length; i++){
+      if (i<5) {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+      } else {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+        console.log('BEFORE',listArr);
+
+      }
+    }
+    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
+
+    var fullString = listArr.join('');
+    list.innerHTML = fullString;
+    document.body.appendChild(list);
+    console.log('AFTER',listArr);
+
+  }
+
+};
+
+capitolHill.calculateCustomersPerHour();
+capitolHill.calculateCookiesSoldEachHour();
+capitolHill.listThings();
+
+
+
+
+
+var alki = {
+
+  name: 'Alki',
+  min: 2,
+  max: 16,
+  avgCookiesPerSale: 4.6,
+  cookiesSoldEachHour: [],
+  avgCustArr: [],
+  totalCooks: 0,
+  //Constructor for all stores
+  calculateCustomersPerHour: function () {
+    for (var i = 0; i < workHours.length; i++) {
+      var randomAmount = Math.floor(Math.random() * (this.max - this.min)) + this.min;
+      this.avgCustArr.push(randomAmount);
+      console.log(this.avgCustArr);
+    }
+  },
+
+  calculateCookiesSoldEachHour: function () {
+    for (var j = 0; j < workHours.length; j++) {
+      this.totalCooks += this.avgCustArr[j];
+      console.log('***********', this.totalCooks)
+    }
+  },
+
+  listThings: function () {
+    var container = document.createElement('div')
+    container.innerHTML = '<h3>' + this.name + '<h3>'
+    document.body.appendChild(container);
+
+    var list = document.createElement('ul')
+    var listArr = [];
+
+    for (var i = 0; i < workHours.length; i++){
+      if (i<5) {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+      } else {
+        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
+        console.log('BEFORE',listArr);
+
+      }
+    }
+    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
+
+    var fullString = listArr.join('');
+    list.innerHTML = fullString;
+    document.body.appendChild(list);
+    console.log('AFTER',listArr);
+
+  }
+
+};
+
+alki.calculateCustomersPerHour();
+alki.calculateCookiesSoldEachHour();
+alki.listThings();
