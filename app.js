939183af148ab1a93cb922c6d25c0eae81ebdced
Johannes Michael <janimc@gmail.com>
2018-10-06 16:05:30 -0700

    constructor and formes added

--- app.js
+++ app.js
@@ -1,238 +1,159 @@
 'use strict';
 
-var workHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm']
-var pikePlace = {
+//overview of table construction
+// 1. container variable
+// 2. new element
+// 3. Give element content
+// 4. append newly created element to container
 
-  name: 'First and Pike',
-  min: 23,
-  max: 65,
-  avgCookiesPerSale: 6.3,
-  cookiesSoldEachHour: [],
-  avgCustArr: [],
-  totalCooks: 0,
-  //Constructor for all stores
-  calculateCustomersPerHour: function () {
-    for (var i = 0; i < workHours.length; i++) {
-      var randomAmount = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.avgCustArr.push(randomAmount);
-      console.log(this.avgCustArr);
-    }
-  },
+//Array for hours open
+//STRETCH: make the # of hours dynamic
+//need to take array and plug it into a function that prints out into the table, similar to how calcCookSold gets pushed into the table
+var hoursOpen = ['Locations','6:00a', '7:00a','8:00a','9:00a','10:00a','11:00a','12:00p','13:00p','14:00p','15:00p','16:00p','17:00p','18:00p','19:00p','20:00p']; 
+var allStoreContainer = [];
 
-  calculateCookiesSoldEachHour: function () {
-    for (var j = 0; j < workHours.length; j++) {
-      this.totalCooks += this.avgCustArr[j];
-      console.log('***********', this.totalCooks)
-    }
-  },
+/*10-01 notes from class
+- change name of hoursOpen array to something like headerRow
+- commenting is great!
+- steps excellent! rendering 'li's can backfire with plugging in new elements in rendering
+- fixing headerHours, likely will have to rename as well
+    -fixing variable name for thEl
+- think about code in English
+- adding container push to constructor helps with DRY
+*/
+//Store constructor function
+var Store = function(name, minCust, maxCust, avgCook){
+  this.name = name; //name of store
+  this.minCust = minCust; //minimum customers per hour
+  this.maxCust = maxCust; //maximum customers per hour
+  this.avgCook = avgCook; //# of average cookies sold per customer
+  this.cookSold = []; //# of cookies sold at this store per hour
 
-  listThings: function () {
-    var container = document.createElement('div')
-    container.innerHTML = '<h3>' + this.name + '<h3>'
-    document.body.appendChild(container);
-
-    var list = document.createElement('ul')
-    var listArr = [];
-
-    for (var i = 0; i < workHours.length; i++){
-      if (i<5) {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-      } else {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-        console.log('BEFORE',listArr);
-
-      }
-    }
-    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
-
-    var fullString = listArr.join('');
-    list.innerHTML = fullString;
-    document.body.appendChild(list);
-    console.log('AFTER',listArr);
-
-  }
-
+  allStoreContainer.push(this);
 };
 
-pikePlace.calculateCustomersPerHour();
-pikePlace.calculateCookiesSoldEachHour();
-pikePlace.listThings();
+var storeForm = document.getElementById('newCookStoreForm');
 
 
-var seatacAirport = {
+// var storesContainer = document.getElementById('cookTable');
 
-  name: 'SeaTac Airport',
-  min: 3,
-  max: 24,
-  avgCookiesPerSale: 1.2,
-  cookiesSoldEachHour: [],
-  avgCustArr: [],
-  totalCooks: 0,
-  //Constructor for all stores
-  calculateCustomersPerHour: function () {
-    for (var i = 0; i < workHours.length; i++) {
-      var randomAmount = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.avgCustArr.push(randomAmount);
-      console.log(this.avgCustArr);
-    }
-  },
+// for(var i in hoursOpen.length){
+//   var thEl = document.createElement('th');
+ 
+//   //thEl = document.createElement('th');
+//   tdEl.textContent = hoursOpen[i];
+//   trEl.appendChild(tdEl);
+// }
+// storesContainer.appendChild(trEl);
 
-  calculateCookiesSoldEachHour: function () {
-    for (var j = 0; j < workHours.length; j++) {
-      this.totalCooks += this.avgCustArr[j];
-      console.log('***********', this.totalCooks)
-    }
-  },
+// function to do hours in first line of cookTable
+var headerHours = function () {
+  // 1. container variable
+  var storesContainer = document.getElementById('cookTable');
 
-  listThings: function () {
-    var container = document.createElement('div')
-    container.innerHTML = '<h3>' + this.name + '<h3>'
-    document.body.appendChild(container);
+ 
 
-    var list = document.createElement('ul')
-    var listArr = [];
+  // 2. new elements
+  var theadEl = document.createElement('thead');
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
 
-    for (var i = 0; i < workHours.length; i++){
-      if (i<5) {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-      } else {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-        console.log('BEFORE',listArr);
-
-      }
-    }
-    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
-
-    var fullString = listArr.join('');
-    list.innerHTML = fullString;
-    document.body.appendChild(list);
-    console.log('AFTER',listArr);
-
+  // 3. give element content
+  for(var i in hoursOpen){
+    thEl = document.createElement('th');  //reassigning variable here essential! "making a 'new' poster instead of rewriting the same one" reusing a variable requires a reassignment
+    thEl.textContent = hoursOpen[i];
+    //console.log(hoursOpen[i]);
+    trEl.appendChild(thEl);
   }
-
+  // 4. append newly created elements to container
+  theadEl.appendChild(trEl);
+  storesContainer.appendChild(theadEl);
 };
 
-seatacAirport.calculateCustomersPerHour();
-seatacAirport.calculateCookiesSoldEachHour();
-seatacAirport.listThings();
-
-
-
-
-var capitolHill = {
-
-  name: 'Capitol Hill',
-  min: 20,
-  max: 38,
-  avgCookiesPerSale: 2.3,
-  cookiesSoldEachHour: [],
-  avgCustArr: [],
-  totalCooks: 0,
-  //Constructor for all stores
-  calculateCustomersPerHour: function () {
-    for (var i = 0; i < workHours.length; i++) {
-      var randomAmount = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.avgCustArr.push(randomAmount);
-      console.log(this.avgCustArr);
-    }
-  },
-
-  calculateCookiesSoldEachHour: function () {
-    for (var j = 0; j < workHours.length; j++) {
-      this.totalCooks += this.avgCustArr[j];
-      console.log('***********', this.totalCooks)
-    }
-  },
-
-  listThings: function () {
-    var container = document.createElement('div')
-    container.innerHTML = '<h3>' + this.name + '<h3>'
-    document.body.appendChild(container);
-
-    var list = document.createElement('ul')
-    var listArr = [];
-
-    for (var i = 0; i < workHours.length; i++){
-      if (i<5) {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-      } else {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-        console.log('BEFORE',listArr);
-
-      }
-    }
-    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
-
-    var fullString = listArr.join('');
-    list.innerHTML = fullString;
-    document.body.appendChild(list);
-    console.log('AFTER',listArr);
-
-  }
-
+// one hour of salcooks sold, produced by random number
+Store.prototype.calcCustPerHour = function(){
+  var randomAmount = Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
+  return Math.round((randomAmount) * this.avgCook);
 };
 
-capitolHill.calculateCustomersPerHour();
-capitolHill.calculateCookiesSoldEachHour();
-capitolHill.listThings();
-
-
-
-
-
-var alki = {
-
-  name: 'Alki',
-  min: 2,
-  max: 16,
-  avgCookiesPerSale: 4.6,
-  cookiesSoldEachHour: [],
-  avgCustArr: [],
-  totalCooks: 0,
-  //Constructor for all stores
-  calculateCustomersPerHour: function () {
-    for (var i = 0; i < workHours.length; i++) {
-      var randomAmount = Math.floor(Math.random() * (this.max - this.min)) + this.min;
-      this.avgCustArr.push(randomAmount);
-      console.log(this.avgCustArr);
-    }
-  },
-
-  calculateCookiesSoldEachHour: function () {
-    for (var j = 0; j < workHours.length; j++) {
-      this.totalCooks += this.avgCustArr[j];
-      console.log('***********', this.totalCooks)
-    }
-  },
-
-  listThings: function () {
-    var container = document.createElement('div')
-    container.innerHTML = '<h3>' + this.name + '<h3>'
-    document.body.appendChild(container);
-
-    var list = document.createElement('ul')
-    var listArr = [];
-
-    for (var i = 0; i < workHours.length; i++){
-      if (i<5) {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-      } else {
-        listArr.push('<li>' + workHours[i] + ': ' + this.avgCustArr[i]+ ' cookies</li>');
-        console.log('BEFORE',listArr);
-
-      }
-    }
-    listArr.push('<li>Total: ' +this.totalCooks + ' cookies</li>');
-
-    var fullString = listArr.join('');
-    list.innerHTML = fullString;
-    document.body.appendChild(list);
-    console.log('AFTER',listArr);
-
+Store.prototype.calcCookSold = function(){ //calculating cookies sold each hour
+  for(var i = 0; i < 15; i++){
+    /* other students did this.hoursOpen to calculate how many hours/day store was open */
+    this.cookSold.push(this.calcCustPerHour());
   }
-
 };
 
-alki.calculateCustomersPerHour();
-alki.calculateCookiesSoldEachHour();
-alki.listThings();
+// make function for cookSold data in each cell
+Store.prototype.cookSoldData = function(){
+  this.calcCookSold();
+
+  // 1. container variable
+  var storesContainer = document.getElementById('cookTable');
+
+  // 2. new element
+  var trEl = document.createElement('tr');
+
+  // 3. Give element content
+  var thEl = document.createElement('th'); //table header
+  thEl.textContent = this.name;
+  trEl.appendChild(thEl); //append table header to row
+
+  //giving table td of avg# of purchased cookies per cust
+  for (var i in this.cookSold){ //append minCust to tds
+    var tdEl = document.createElement('td');
+    tdEl.textContent = this.cookSold[i];
+    trEl.appendChild(tdEl);
+  }
+  // 4. append newly created element to container
+  storesContainer.appendChild(trEl);
+};
+
+//Function to figure out Total row
+var totals = function(){
+  var storesContainer = document.getElementById('cooktable');
+  var trEl = document.createElement('tr');
+  var tdEl = document.createElement('td');
+
+  for(var i in Store.calcCookSold/* total of all stores[i] */) {
+    tdEl.textContent = this.cookSold[i];
+    trEl.appendChild(tdEl);
+  }
+  storesContainer.appendChild(trEl);
+};
+
+//=======================
+// Function that renders all the stores
+var allCookStores = [];
+var renderAllStores = function(){
+  allCookStores.push(pikes.cookSoldData(), capHill.cookSoldData(),seaTac.cookSoldData(),seaCtr.cookSoldData(),alki.cookSoldData());
+};
+
+// store constructor data
+var pikes = new Store('1st and Pike',23,65,6.3,[]);
+var capHill = new Store('Capitol Hill',20,38,2.3,[]);
+var seaTac = new Store('SeaTac Airport',3,24,1.2,[]);
+var seaCtr = new Store('Seattle Center',11,38,3.7,[]);
+var alki = new Store('Alki',2,16,4.6,[]);
+
+// function calls
+headerHours();
+renderAllStores();
+//totals();
+
+// Forms
+
+var handleMakeStore = function(eventStore){
+  eventStore.preventDefault(); //prevents page from refreshing
+  eventStore.stopPropagation();
+  // putting down sample variables for now, though I figure I can get better integration with more direct calls to Store properties.
+  var storeName = eventStore.target.storeName.value;
+  var minCustomers = parseInt(eventStore.target.minCustomers.value);
+  var maxCustomers = parseInt(eventStore.target['max-Customers'].value);
+  var averageCookies = parseInt(eventStore.target.averageCookies.value);
+  var newStore = new Store(storeName, minCustomers, maxCustomers, averageCookies);
+  // allStoreContainer.push(newStore);
+  console.log(allCookStores);
+};
+// likely need other code to make the table footer/total dynamic and populate new numbers that include the new store
+
+storeForm.addEventListener('submit', handleMakeStore);
